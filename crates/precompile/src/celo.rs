6506fa7adb2c992a6fb3a73181e2b1efdb585268
Daan Middelkoop <daantje676@gmail.com>
2023-12-10 22:01:31 +0100

    Fix tiny bugs in the precompile oops

--- crates/precompile/src/celo.rs
+++ crates/precompile/src/celo.rs
@@ -6,9 +6,10 @@ use crate::{
 pub mod celo {
     use core::str::FromStr;
 
+    use revm_primitives::B256;
+
     use super::*;
 
-    const ZERO_ADDRESS: Address = crate::u64_to_address(0);
     const ADDRESS: Address = crate::u64_to_address(0xFF - 2);
 
 
@@ -23,20 +24,20 @@ pub mod celo {
                 return Err(PrecompileError::CustomError("Wrong data input length".to_string()));
             }
 
-            if caller != Address::from_str("0xcB8710e072aC4700eE7eD0C63B2f2102366a7a39").unwrap() {
+            if caller != Address::from_str("0x471ece3750da237f93b8e339c536989b8978a438").unwrap() {
                 return Err(PrecompileError::CustomError("Unable to call transfer from unpermissioned address".to_string()));
             }
 
-            let from = Address::from_slice(&input[0..32]);
-            let to = Address::from_slice(&input[32..64]);
+            let from = Address::from_slice(&input[12..32]);
+            let to = Address::from_slice(&input[44..64]);
 
-            let value = U256::from_le_slice(&input[64..96]);
+            let value = U256::from_be_slice(&input[64..96]);
 
-            if from == ZERO_ADDRESS {
+            if from == Address::ZERO {
                 host.add_balance(&to, value)?;
             } else {
                 if host.get_balance(from)? < value {
-                    return Err(PrecompileError::CustomError("Unable to call transfer from unpermissioned address".to_string()));
+                    return Err(PrecompileError::CustomError(format!("Address {from:?} lacks balance: {}, transfer value: {}", host.get_balance(from)?, value)));
                 }
                 host.transfer(&from, &to, value)?;
             }
